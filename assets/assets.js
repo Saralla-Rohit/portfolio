014029f360067842766c065aeb88b009fc2dce26
rohit sarla <rohitsarla69@gmail.com>
2025-01-27 11:08:38 +0530

    portfolio

---
+++ assets/assets.js
@@ -0,0 +1,136 @@
+import user_image from './profile pic.jpg';
+import code_icon from './code-icon.png';
+import code_icon_dark from './code-icon-dark.png';
+import edu_icon from './edu-icon.png';
+import edu_icon_dark from './edu-icon-dark.png';
+import project_icon from './project-icon.png';
+import project_icon_dark from './project-icon-dark.png';
+import express from './express.png';
+import react from './react.png';
+import nodejs from './nodejs.png';
+import jquery from './jquery.png';
+import javascript from './javascript.png';
+import bootstrap from './bootstrap.png';
+import git from './git.png';
+import mongodb from './mongodb.png';
+import right_arrow_white from './right-arrow-white.png';
+import logo from './logo-1.jpg';
+import logo_dark from './logo_dark-1.png';
+import mail_icon from './mail_icon.png';
+import mail_icon_dark from './mail_icon_dark.png';
+import profile_img from './profile pic.jpg';
+import download_icon from './download-icon.png';
+import hand_icon from './hand-icon.png';
+import header_bg_color from './header-bg-color.png';
+import moon_icon from './moon_icon.png';
+import sun_icon from './sun_icon.png';
+import arrow_icon from './arrow-icon.png';
+import arrow_icon_dark from './arrow-icon-dark.png';
+import menu_black from './menu-black.png';
+import menu_white from './menu-white.png';
+import close_black from './close-black.png';
+import close_white from './close-white.png';
+import web_icon from './web-icon.png';
+import mobile_icon from './mobile-icon.png';
+import ui_icon from './ui-icon.png';
+import graphics_icon from './graphics-icon.png';
+import right_arrow from './right-arrow.png';
+import send_icon from './send-icon.png';
+import right_arrow_bold from './right-arrow-bold.png';
+import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+
+export const assets = {
+    user_image,
+    code_icon,
+    code_icon_dark,
+    edu_icon,
+    edu_icon_dark,
+    project_icon,
+    project_icon_dark,
+    express,
+    react,
+    nodejs,
+    jquery,
+    javascript,
+    bootstrap,
+    git,
+    mongodb,
+    right_arrow_white,
+    logo,
+    logo_dark,
+    mail_icon,
+    mail_icon_dark,
+    profile_img,
+    download_icon,
+    hand_icon,
+    header_bg_color,
+    moon_icon,
+    sun_icon,
+    arrow_icon,
+    arrow_icon_dark,
+    menu_black,
+    menu_white,
+    close_black,
+    close_white,
+    web_icon,
+    mobile_icon,
+    ui_icon,
+    graphics_icon,
+    right_arrow,
+    send_icon,
+    right_arrow_bold,
+    right_arrow_bold_dark
+};
+
+export const workData = [
+    
+    {
+        title: 'Local service provider',
+        description: 'Find local service providers near me',
+        bgImage: '/work-2.png',
+        link: 'https://service-hunt.onrender.com' 
+    },
+    {
+        title: 'Roomify',
+        description: 'Organize and compare room options',
+        bgImage: '/work-1.png',
+        link: 'https://room-search-and-management.onrender.com' 
+    },
+    {
+        title: 'Tube Ratio',
+        description: 'Watch high rated videos on youtube',
+        bgImage: '/work-3.png',
+        link: 'https://youtube-clone-like-ratio.onrender.com' 
+    },
+]
+
+export const serviceData = [
+    { 
+        icon: assets.mobile_icon, 
+        title: 'Full-Stack Web Development', 
+        description: 'Developing robust applications using the MERN stack (MongoDB, Express, React, Node.js) to create seamless user experiences and scalable back-end solutions.', 
+        link: '' 
+    },
+    { 
+        icon: assets.web_icon, 
+        title: 'Web Design', 
+        description: 'Building responsive and scalable web applications using HTML, CSS, JavaScript, and React.', 
+        link: '' 
+    },
+    { 
+        icon: assets.graphics_icon, 
+        title: 'API Development', 
+        description: 'Designing and implementing RESTful APIs to enable smooth communication between front-end and back-end systems.', 
+        link: '' 
+    },
+];
+
+export const infoList = [
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'MongoDb, Express, React, Node [MERN]' },
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Computer Science' },
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
+];
+
+export const toolsData = [
+    assets.mongodb, assets.express, assets.react, assets.nodejs, assets.jquery, assets.javascript, assets.bootstrap, assets.git
+];
