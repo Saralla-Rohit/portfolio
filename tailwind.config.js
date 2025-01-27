0e5c48cb4d6b54ff66c8ccf1656c52989fbbbdff
rohit sarla <rohitsarla69@gmail.com>
2025-01-27 13:50:30 +0530

    retreived the correct code

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,20 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
+      colors: {
+        darkTheme: '#121212',
+        darkHover: '#2A2A2A',
+        lightHover: '#F5F5F5'
+      },
+      gridTemplateColumns: {
+        'auto': 'repeat(auto-fit, minmax(300px, 1fr))'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
